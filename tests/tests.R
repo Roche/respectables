3b70f0238a6bbf92ab392299a6c029782bb71d23
Adrian Waddell <adrian.waddell@roche.com>
2021-05-07 20:00:55 +0200

    remove tinytest dependency

--- tests/tests.R
+++ tests/tests.R
@@ -1,25 +1,26 @@
-library(tinytest)
-require(tibble)
-library(respectables)
-noop_fun = respectables:::noop_func_constr("INDEP")
-badrec1 <- tibble(variables = "MISSINGDEP", dependencies = "MISSING", func = list(function(n, .df, ...) rep("", n)), func_args = list(NULL))
-expect_error(validate_recipe_deps(badrec1), "No independent recipe components found")
-badrec2 <- tribble(~variables, ~dependencies, ~func, ~func_args,
-                   "INDEP", no_deps, noop_fun, NULL,
-                   "CDEP1", "CDEP2", noop_fun, NULL,
-                   "CDEP2", "CDEP1", noop_fun, NULL)
-
-expect_error(validate_recipe_deps(badrec2), "Unable to generate some dependent variables: CDEP1, CDEP2")
-
-badrec3 <- tibble(variables = list("REAL", character()), list(noop_fun, noop_fun), list(NULL, NULL))
-expect_error(validate_recipe_deps(badrec3), "Bad variable specification")
-badrec4 <- rbind(badrec3[1,], badrec3[1,])
-expect_error(validate_recipe_deps(badrec4), "Repeated variable")
-
-goodrec <- tribble(~variables, ~dependencies, ~func, ~func_args, ~keep,
-                   "VAR1", no_deps, seq_n, NULL, TRUE)
-
-goodrec2 <- tribble(~variables, ~dependencies, ~func, ~func_args, ~keep,
-                   "VAR1", no_deps, seq_n, NULL, TRUE)
-
-
+# library(tinytest)
+# require(tibble)
+# library(respectables)
+# noop_fun = respectables:::noop_func_constr("INDEP")
+# badrec1 <- tibble(variables = "MISSINGDEP", dependencies = "MISSING", func = list(function(n, .df, ...) rep("", n)), func_args = list(NULL))
+# expect_error(validate_recipe_deps(badrec1), "No independent recipe components found")
+# badrec2 <- tribble(~variables, ~dependencies, ~func, ~func_args,
+#                    "INDEP", no_deps, noop_fun, NULL,
+#                    "CDEP1", "CDEP2", noop_fun, NULL,
+#                    "CDEP2", "CDEP1", noop_fun, NULL)
+#
+# expect_error(validate_recipe_deps(badrec2), "Unable to generate some dependent variables: CDEP1, CDEP2")
+#
+# badrec3 <- tibble(variables = list("REAL", character()), list(noop_fun, noop_fun), list(NULL, NULL))
+# expect_error(validate_recipe_deps(badrec3), "Bad variable specification")
+# badrec4 <- rbind(badrec3[1,], badrec3[1,])
+# expect_error(validate_recipe_deps(badrec4), "Repeated variable")
+#
+# goodrec <- tribble(~variables, ~dependencies, ~func, ~func_args, ~keep,
+#                    "VAR1", no_deps, seq_n, NULL, TRUE)
+#
+# goodrec2 <- tribble(~variables, ~dependencies, ~func, ~func_args, ~keep,
+#                    "VAR1", no_deps, seq_n, NULL, TRUE)
+#
+#
+#
